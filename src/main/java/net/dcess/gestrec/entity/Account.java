6d237db776e59a0db68880587f5920ce233606ab
Ahmed Aytouna <aaytouna@everis.com>
2022-02-23 20:38:52 +0100

    Add Account Class

--- src/main/java/net/dcess/gestrec/entity/Account.java
+++ src/main/java/net/dcess/gestrec/entity/Account.java
@@ -4,10 +4,50 @@
  */
 package net.dcess.gestrec.entity;
 
-/**
- *
- * @author aaytouna
- */
-public class Account {
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Account implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Column(nullable = false)
+    @NotEmpty
+    private String username;
+
+    @Column(nullable = false)
+    @NotEmpty
+    private String password;
+
+    @Column(nullable = false)
+    @NotEmpty
+    private String firstName;
+
+    @Column(nullable = false)
+    @NotEmpty
+    private String lastName;
+
+    @Column(nullable = false)
+    @NotEmpty
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    private String accountId;
 }
