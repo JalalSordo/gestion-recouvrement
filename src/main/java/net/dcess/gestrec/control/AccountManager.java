e0ad961513e83b7a715cc1a108685a5e24593729
FO <FO@LAPTOP-GAVH8MH4>
2022-02-24 00:54:54 +0100

    Update AccountManager.java

--- src/main/java/net/dcess/gestrec/control/AccountManager.java
+++ src/main/java/net/dcess/gestrec/control/AccountManager.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package net.dcess.gestrec.control;
 
 import java.util.List;
